96f098580de6c7f091deda892a8bfef818d6ea79
eluraschi <Eduardoluraschi@gmail.com>
2021-05-24 00:01:10 -0300

    ok

---
+++ ReclamosAlcaldia/ConsolaPrueba/Program.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaPrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PruebaConexion pruebaCarlos = new PruebaConexion();
+
+            pruebaCarlos.AgregarTipoReclamo();
+
+        }
+    }
+}
---
+++ ReclamosAlcaldia/DataAccess/Repository/PruebaConexion.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccess.Repository
+{
+    public class PruebaConexion
+    {
+
+        public void AgregarTipoReclamo()
+        {
+            using (ReclamosAlcaldiaEntities context = new ReclamosAlcaldiaEntities())
+            {
+                TipoDeReclamo nuevoTipoReclamo = new TipoDeReclamo();
+
+                nuevoTipoReclamo.nombre = "tipoRecPueba";
+                nuevoTipoReclamo.descripcion = "descripcion";
+                context.TipoDeReclamo.Add(nuevoTipoReclamo);
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
